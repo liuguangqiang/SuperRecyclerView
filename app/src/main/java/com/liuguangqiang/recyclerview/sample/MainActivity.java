a2ea0840491cc14800976375dc47203d9bfa194c
Eric <guangqiang.dev@gmail.com>
2015-07-07 10:30:59 +0800

    create the library.

--- app/src/main/java/com/liuguangqiang/recyclerview/sample/MainActivity.java
+++ app/src/main/java/com/liuguangqiang/recyclerview/sample/MainActivity.java
@@ -1,4 +1,4 @@
-package com.liuguangqiang.recyclerview;
+package com.liuguangqiang.recyclerview.sample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
---
+++ library/src/main/java/com/liuguangqiang/recyclerview/listener/OnPageListener.java
@@ -0,0 +1,10 @@
+package com.liuguangqiang.recyclerview.listener;
+
+/**
+ * Created by Eric on 15/7/7.
+ */
+public interface OnPageListener {
+
+    void onPage();
+
+}
