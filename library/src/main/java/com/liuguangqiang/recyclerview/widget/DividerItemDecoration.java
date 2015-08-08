a7d0c5cf29723e6d16166057f383a007b105c97f
Eric <guangqiang.dev@gmail.com>
2015-08-08 20:34:30 +0800

    added a custorm ItemDecoration for showing a divider between all items.

---
+++ library/src/main/java/com/liuguangqiang/recyclerview/widget/DividerItemDecoration.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright 2015 Eric Liu
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.liuguangqiang.recyclerview.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Add a mDivider between all items for RecyclerView.
+ * <p>
+ * Created by Eric on 15/8/7.
+ */
+public class DividerItemDecoration extends RecyclerView.ItemDecoration {
+
+    private static final int[] ATTRS = new int[]{
+            android.R.attr.listDivider
+    };
+
+    private Drawable mDivider;
+
+    public DividerItemDecoration(Context context) {
+        final TypedArray typedArray = context.obtainStyledAttributes(ATTRS);
+        mDivider = typedArray.getDrawable(0);
+        typedArray.recycle();
+    }
+
+    public DividerItemDecoration(Drawable divider) {
+        this.mDivider = divider;
+    }
+
+    @Override
+    public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+        super.onDraw(c, parent, state);
+        drawDivider(c, parent);
+    }
+
+    public void drawDivider(Canvas c, RecyclerView parent) {
+        final int left = parent.getPaddingLeft();
+        final int right = parent.getWidth() - parent.getPaddingRight();
+
+        final int childCount = parent.getChildCount();
+        for (int i = 0; i < childCount; i++) {
+            final View child = parent.getChildAt(i);
+            final RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child
+                    .getLayoutParams();
+            final int top = child.getBottom() + params.bottomMargin;
+            final int bottom = top + mDivider.getIntrinsicHeight();
+            mDivider.setBounds(left, top, right, bottom);
+            mDivider.draw(c);
+        }
+    }
+
+}
