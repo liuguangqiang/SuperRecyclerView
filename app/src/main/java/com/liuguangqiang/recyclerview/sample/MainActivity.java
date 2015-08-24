39bffbf68b7eb0d3235307329a3e8aaf6b30821b
Eric <guangqiang.dev@gmail.com>
2015-08-24 13:38:27 +0800

    update sample.

--- app/src/main/java/com/liuguangqiang/recyclerview/sample/MainActivity.java
+++ app/src/main/java/com/liuguangqiang/recyclerview/sample/MainActivity.java
@@ -1,12 +1,31 @@
+/*
+ * Copyright 2015 Eric Liu
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package com.liuguangqiang.recyclerview.sample;
 
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.liuguangqiang.asyncokhttp.AsyncOkHttp;
 import com.liuguangqiang.asyncokhttp.JsonResponseHandler;
 import com.liuguangqiang.recyclerview.listener.OnPageListener;
+import com.liuguangqiang.recyclerview.widget.DividerItemDecoration;
+import com.liuguangqiang.recyclerview.widget.LinearRecyclerView;
 import com.liuguangqiang.recyclerview.widget.SuperRecyclerView;
 
 import java.util.ArrayList;
@@ -33,6 +52,18 @@ public class MainActivity extends AppCompatActivity implements OnPageListener {
         adapter = new StoryAdapter(getApplicationContext(), data);
         recyclerView.setAdapter(adapter);
         recyclerView.setOnPageListener(this);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getApplicationContext()));
+        recyclerView.setOnScrollListener(new LinearRecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
+
+            }
+
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+
+            }
+        });
     }
 
     @Override
